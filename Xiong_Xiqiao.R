a417f4f71a472a218d30b1676bc3173b07eb1613
xiongxiqiao <xiqiao.xiong@uconn.edu>
2017-09-11 22:30:20 -0400

    Add files via upload

---
+++ Xiong_Xiqiao.R
@@ -0,0 +1,24 @@
+options(max.print=10000000)
+options(scipen = 999)
+st <- system.time( {
+a<-seq(3,2000001,by=2)
+b<-(a^2-1)/2
+c<-(a^2+1)/2
+res <- array(
+  c(a,b,c),
+  dim = c(1000000, 3),
+  dimnames = list(
+    c(seq(1,1000000)),
+    c("a", "b", "c")
+  )
+)
+res
+}
+)
+
+cat("Xiong Xiqiao \n")
+cat(dim(res),"\n")
+cat(min(res),max(res),"\n")
+cat(sum(res[,1]^2 + res[,2]^2 - res[,3]^2), "\n")
+print(st)
+
